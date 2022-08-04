3287ea3f16305d7d455fd73e908dcff9418039fc
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-08-04 03:27:36 -0300

    integracion con mongodb

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const app = require("./server");
 const http = require("http").createServer(app);
+const mongoose = require("mongoose");
+const url = "mongodb://0.0.0.0:27017/mongo_clase";
 
 const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
 });
+mongoose
+  .connect(url, {})
+  .then(() => console.log("Conectado a mongo"))
+  .catch((e) => console.log("Error de conexion: " + e));
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,60 +1,70 @@
-let usuario = [
-  {
-    id: 0,
-    nombre: "pepe",
-  },
-  {
-    id: 1,
-    nombre: "juan",
-  },
-  {
-    id: 2,
-    nombre: "jony",
-  },
-];
+const mongoose = require("mongoose");
+
+const personaSchema = mongoose.Schema({
+  nombre: String,
+  edad: Number,
+  pais: String,
+});
+
+const PersonaModel = mongoose.model("personas", personaSchema);
 
 const index = async (req, res) => {
-  return res.render("../src/views/usuario/index", { usuario });
+  const personas = await PersonaModel.find();
+  return res.render("../src/views/usuario/index", { personas });
 };
 
 const show = async (req, res) => {
   const id = req.params.id;
-  const elemento = usuario[id];
-  return res.render("../src/views/usuario/show", { elemento });
+  const personas = await PersonaModel.find({ _id: id });
+  return res.render("../src/views/usuario/show", { personas });
 };
 
 const edit = async (req, res) => {
   const id = req.params.id;
-  const elemento = usuario[id];
-  return res.render("../src/views/usuario/edit", { elemento });
+  const personas = await PersonaModel.find({ _id: id });
+  return res.render("../src/views/usuario/edit", { personas });
 };
 
 const create = async (req, res) => {
-  return res.render("../src/views/usuario/create");
+  const persona = new PersonaModel({
+    nombre: nombre,
+    edad: edad,
+    pais: pais,
+  });
+  return res.render("../src/views/usuario/create", { persona });
 };
 
 // API
 
 const store = async (req, res) => {
-  const { nombre, id } = req.body;
-  if (nombre) {
-    usuario.unshift({ id, nombre });
+  const { nombre, edad, pais } = req.body;
+  const persona = new PersonaModel({
+    nombre: nombre,
+    edad: edad,
+    pais: pais,
+  });
+  if (persona) {
     return res
       .status(200)
-      .json({ status: 200, id, nombre, msg: "Creado correctamente" });
+      .json({ status: 200, msg: "Creado correctamente", persona });
   } else {
     return res.status(404).json({ msg: "No se recibieron los datos" });
   }
 };
 
 const update = async (req, res) => {
-  const { nombre } = req.body;
+  const { nombre, edad, pais } = req.body;
   const id = req.params.id;
+  const persona = await PersonaModel.updateOne(
+    { _id: id },
+    {
+      $set: { nombre: nombre, edad: edad, pais: pais },
+    }
+  );
   if (id) {
-    const actualizado = usuario.splice(id, 1, { id: id, nombre: nombre });
     return res
       .status(201)
-      .json({ status: 201, actualizado, msg: "Editado correctamente" });
+      .json({ status: 201, persona, msg: "Editado correctamente" });
   } else {
     return res.status(404).json({ msg: "No se recibieron los datos" });
   }
@@ -62,10 +72,10 @@ const update = async (req, res) => {
 
 const destroy = async (req, res) => {
   const id = req.params.id;
-  const eliminado = usuario.splice(id, 1);
+  const persona = PersonaModel.deleteOne({ _id: id });
   return res
     .status(200)
-    .json({ status: 200, eliminado, msg: "Eliminado correctamente" });
+    .json({ status: 200, persona, msg: "Eliminado correctamente" });
 };
 
 module.exports = {
